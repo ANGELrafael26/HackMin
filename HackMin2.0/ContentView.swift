bb8a0f9578be08b3c82fd636cee2b6e35074adf6
Jesús Ortega <jesus.ortegaa1224@gmail.com>
2026-05-07 23:47:41 -0600

    Initial Commit

---
+++ HackMin2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackMin2.0
+//
+//  Created by Jesus Ortega on 07/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackMin2.0/HackMin2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  HackMin2_0App.swift
+//  HackMin2.0
+//
+//  Created by Jesus Ortega on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct HackMin2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
